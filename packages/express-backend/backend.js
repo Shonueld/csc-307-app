bdec102856e71b9229c1a326cd065a31d60d0aa0
Shonueld <seangriffindev@gmail.com>
2025-04-21 13:55:24 -0700

    frontend can now talk to backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -52,7 +54,6 @@ app.delete("/users/:id", (req, res) => {
     res.status(204).send(); // No content
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
